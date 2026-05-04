6b7f95218d2fb483ea9d659ebff28e983edf9268
Thiago <t.thiago@escolar.ifrn.edu.br>
2026-05-04 10:31:22 -0300

    continuação atv

--- Loja/loja/admin.py
+++ Loja/loja/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import * #imporata nossos models
+
+class FabricanteAdmin(admin.ModelAdmin):
+    # Cria um filtro de hierarquia com datas
+    date_hierarchy = 'criado_em'
+
 admin.site.register(Fabricante) #adiciona a interface do adm
 admin.site.register(Categoria)
 admin.site.register(Produto)
+
